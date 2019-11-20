f9b1655e3612e5ec413a0eae1cbd34eee025fd21
Ryandi Tjia <ryandi.tjia@me.com>
2019-11-20 21:12:39 +0700

    Add sideEffects false to package.json. Refactor export

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,21 @@
 // Hooks
-import { useOnScreen } from './hooks/useOnScreen'
+export { useOnScreen } from './hooks/useOnScreen'
 // Mixins
-import { scrimGradient } from './mixins/scrim-gradient'
+export { scrimGradient } from './mixins/scrim-gradient'
 // React components
-import { ApostrofLogo } from './react-components/ApostrofLogo'
-import { ExternalLink } from './react-components/ExternalLink'
-import { MailtoLink } from './react-components/MailtoLink'
-import { PhoneLink } from './react-components/PhoneLink'
-import { WhatsAppLink } from './react-components/WhatsAppLink'
+export { ApostrofLogo } from './react-components/ApostrofLogo'
+export { ExternalLink } from './react-components/ExternalLink'
+export { MailtoLink } from './react-components/MailtoLink'
+export { PhoneLink } from './react-components/PhoneLink'
+export { WhatsAppLink } from './react-components/WhatsAppLink'
 // Utils
-import { createPhoneNumber } from './utils/create-phone-number'
-import { createWhatsAppLink } from './utils/create-whatsapp-link'
-import { cx } from './utils/cx'
-import { extractNodes } from './utils/extract-nodes'
-import { fakeGraphQLTag } from './utils/fake-graphql-tag'
-import { formatNumber } from './utils/format-number'
-import { hexToRGB } from './utils/hex-to-rgb'
-import { isValidEmail } from './utils/is-valid-email'
-import { setPartiallyCurrent } from './utils/set-partially-current'
-import { submitFormToNetlify } from './utils/submit-form-to-netlify'
-
-export {
-	useOnScreen,
-	scrimGradient,
-	ApostrofLogo,
-	ExternalLink,
-	MailtoLink,
-	PhoneLink,
-	WhatsAppLink,
-	createPhoneNumber,
-	createWhatsAppLink,
-	cx,
-	extractNodes,
-	fakeGraphQLTag,
-	formatNumber,
-	hexToRGB,
-	isValidEmail,
-	setPartiallyCurrent,
-	submitFormToNetlify,
-}
+export { createPhoneNumber } from './utils/create-phone-number'
+export { createWhatsAppLink } from './utils/create-whatsapp-link'
+export { cx } from './utils/cx'
+export { extractNodes } from './utils/extract-nodes'
+export { fakeGraphQLTag } from './utils/fake-graphql-tag'
+export { formatNumber } from './utils/format-number'
+export { hexToRGB } from './utils/hex-to-rgb'
+export { isValidEmail } from './utils/is-valid-email'
+export { setPartiallyCurrent } from './utils/set-partially-current'
+export { submitFormToNetlify } from './utils/submit-form-to-netlify'
